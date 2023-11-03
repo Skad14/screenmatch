d7b6a2e97afc8bca24eb9d7065e8efd4af121b0a
Rafael Franca <rafa-cross@hotmail.com>
2023-11-03 18:24:23 -0300

    Projeto do streaming Screen Match em Java.

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        Movie meuFilme = new Movie();
+
+        meuFilme.nome = "O Poderoso Chefão";
+        meuFilme.anoDeLancamento = 1970;
+        meuFilme.duracaoEmMinutos = 180;
+        meuFilme.incluidoNoPlano = true;
+
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
+
+        System.out.println("Média de avaliações do filme: " +meuFilme.mediaDasAvaliacoes());
+    }
+}
