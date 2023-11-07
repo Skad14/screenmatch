2eb8dea429597791c03a09f702b1745f00239eed
Rafael Franca <rafa-cross@hotmail.com>
2023-11-07 14:07:46 -0300

    Projeto do streaming Screen Match em Java.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
 import br.com.alura.screenmatch.modelos.Filmes;
 import br.com.alura.screenmatch.modelos.Series;
 
@@ -5,19 +6,24 @@ public class Main {
 
     public static void main(String[] args) {
         Filmes meuFilme = new Filmes();
+        Filmes meuFilme2 = new Filmes();
         Series minhaSerie = new Series();
 
         meuFilme.setNome("O Poderoso Chefão");
         meuFilme.setAnoDeLancamento(1970);
         meuFilme.setDuracaoEmMinutos(180);
         meuFilme.setIncluidoNoPlano(true);
-        System.out.println("Duração do filme");
-        System.out.println("Média de avaliações do filme: " +meuFilme.mediaDasAvaliacoes());
 
-        meuFilme.exibeFichaTecnica();
+        meuFilme2.setNome("Interestelar");
+        meuFilme2.setAnoDeLancamento(2013);
+        meuFilme2.setDuracaoEmMinutos(200);
+        meuFilme2.setIncluidoNoPlano(true);
+
         meuFilme.avalia(8);
         meuFilme.avalia(5);
         meuFilme.avalia(10);
+        meuFilme.exibeFichaTecnica();
+        System.out.println("Média das avaliações: " +meuFilme.mediaDasAvaliacoes());
 
         minhaSerie.setNome("Lost");
         minhaSerie.setAnoDeLancamento(2000);
@@ -25,7 +31,12 @@ public class Main {
         minhaSerie.setEpisodiosPorTemporada(10);
         minhaSerie.setMinutosPorEpisodio(50);
         minhaSerie.exibeFichaTecnica();
-        System.out.println("Tempo total da série: " + minhaSerie.getDuracaoEmMinutos() + " minutos");
+        System.out.println("Tempo para maratonar a série: " + minhaSerie.getDuracaoEmMinutos() + " minutos");
 
+        CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
+        calculadora.inclui(meuFilme);
+        calculadora.inclui(meuFilme2);
+        calculadora.inclui(minhaSerie);
+        System.out.println("\nTempo para maratonar filmes e séries da lista: " +calculadora.getTempoTotal());
     }
 }
